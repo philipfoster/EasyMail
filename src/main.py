c47555a2bb6dd9f669dff6f28bd6b01f21bfff5d
Philip <philipfoster462@gmail.com>
2018-08-24 17:38:30 -0500

    Add method to send mail

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 
 @app.route("/")
-def send_mail():
+def test():
     return 'hello world'
 
+
+@app.route("/sendmail", methods=['POST'])
+def send_mail():
+    name = request.form.get('name')
+    from_addr = request.form.get('from')
+    message = request.form.get('message')
+
+    print("name = %s, from = %s, message = %s" % (name, from_addr, message))
+    return ''
+
+
+if __name__ == '__main__':
+    app.run(debug=False, port=5000)
